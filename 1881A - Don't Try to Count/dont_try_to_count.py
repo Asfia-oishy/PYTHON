03a5b933c7b21557b1b8880906f4d6e417ca2e74
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-10 17:29:34 +0600

    accepted

--- 1881A - Don't Try to Count/dont_try_to_count.py
+++ 1881A - Don't Try to Count/dont_try_to_count.py
@@ -3,26 +3,21 @@ T = int(input())
 
 for _ in range(T):
     n,m = map(int,input().split())
-    x = str(input())
-    s = str(input())
-    set1 = set(s)
+    x = str(input()) #length n
+    s = str(input()) #length m
 
     flag = False
     op = 0
     if(s==x):
         print(0)
         continue
-    
-
-    
-
-    while(len(x)<=(5*max(m,n))):
-        #print(x)
-        if (len(x)>=len(s)):
+    cnt =0
+    while(True):
+        if (len(x)>= m):
             start = 0
             end = start + m
 
-            for j in range(len(x)-m):
+            for j in range(len(x)-m+1):
                 st = x[start:end]
                 if st == s:
                     print(op)
@@ -32,7 +27,10 @@ for _ in range(T):
                 end+=1
         if flag==True:
             break
-
+        if len(x)>len(s):
+            cnt+=1
+        if cnt==2:
+            break
         x=x+x
         op+=1
     if flag ==False:
